6a23ebc9e052c287b9a118e8e3ce346c84240ae4
Giacomo Salici <giambo2000@gmail.com>
2022-12-10 19:47:01 +0100

    day04,part2

--- 2022/day04/src1.py
+++ 2022/day04/src1.py
@@ -1,14 +1,17 @@
 import sys
 
 nAssignmentsFullyContained = 0
-with open("./2022/day04/input.txt", "r") as file:
+nAssignmentsOverlapping = 0
+with open(sys.argv[1], "r") as file:
     for line in file:
         assignmentsPair = line.strip().split(",")
         a= [{i for i in range(100) if (i>=int((assignmentsPair[j].split("-"))[0]) and i<=int((assignmentsPair[j].split("-"))[1]))} for j in range(len(assignmentsPair))]
         if a[0].issubset(a[1]) or a[1].issubset(a[0]):
             nAssignmentsFullyContained+=1
+        if len(a[0].intersection(a[1]))!=0:
+            nAssignmentsOverlapping+=1
 
-print(nAssignmentsFullyContained)
+print(nAssignmentsOverlapping)
 
        
 
