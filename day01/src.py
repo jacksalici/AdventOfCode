e5c9f50e166837fcb007df12fc6e887d5a557b3f
Giacomo Salici <giambo2000@gmail.com>
2022-12-10 16:28:19 +0100

    day01part02

--- day01/src.py
+++ day01/src.py
@@ -1,30 +1,14 @@
 import sys
 
-current = {
-    "elf": 1,
-    "weight": 0
- } #elf number, weight
-max = {
-    "elf": 1,
-    "weight": 0
- } 
+weights = [0]
 
 with open(sys.argv[1], "r") as file:
     for line in file:
         if line == '\n':
-            if current["weight"]>max["weight"]:
-                max["weight"] = current["weight"]
-                max["elf"] = current["elf"]
-
-            print (max, current)
-
-            current["elf"] += 1
-            current["weight"] = 0
-
+            weights.append(0)
         else:
-            current["weight"] += int(line.strip())
+            weights[-1] += int(line.strip())
 
-       
-print("MAX = " + str(max["weight"])) 
-
+weights.sort(reverse=True)
+print(weights[0]+weights[1]+weights[2])
 
