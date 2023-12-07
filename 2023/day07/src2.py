a25a84b881d05b7eaa57f558caba9fdaa0234650
Giacomo Salici <giambo2000@gmail.com>
2023-12-07 18:42:53 +0100

    onliner

--- 2023/day07/src2.py
+++ 2023/day07/src2.py
@@ -1,66 +1,17 @@
+#6: Five of a kind, #5: Four of a kind, #4: Full house, #3: Three of a kind #2: Two pair, #1: One pair, #0: High card
 import sys
 from collections import Counter
 
-a = ['A','K','Q','T','9','8','7','6','5','4','3','2','J']
+def score(hand: str):
+    j = hand.count('J')
+    if j == 5 or j == 4: return 6
+    v = sorted(Counter(hand.replace('J', "")).values(), reverse=True)
+    if v[0] + j > 3: return v[0] + j + 1
+    if v[0:2] == [2, 2]: return 4 if j == 1 else 2
+    if v[0:2] == [2, 1]: return 3 if j == 1 else 1
+    if v[0] + j == 3: return 4 if v[1] == 2 else 3
+    return j
 
-#249781879
-hands = []
+print(sum([hand[1] * (index+1) for (index, hand) in enumerate(sorted([(line.split()[0], int(line.split()[1]), score(line.split()[0])) for line in open(sys.argv[1]).readlines()],key=lambda word: (word[2], ['AKQT98765432J'[::-1].index(c) for c in word[0]])))]))
 
-def score(hand):
-    couple = 0
-    triple = 0
-    quaple = 0
-    
-    for s in a: 
-        c = hand.count(s)
-        if c == 5:
-            return 6 
-        if c == 4:
-            quaple += 1
-        if c == 3:
-            triple+=1
-        if c == 2:
-            couple+=1
-            
-    if quaple:
-        return 5        
-
-    if couple == 1 and triple == 1:
-        return 4
-        
-    elif couple == 0 and triple == 1:
-        return 3
-    elif couple == 2 and triple == 0:
-        return 2
-    elif couple == 1:
-        return 1
-    else:
-        return 0
-
-
-def best_score(hand: str):
-    possibilities = []
-    
-    for J in a[:-1]:
-        possibilities.append(score(hand.replace("J", J)))
-    
-    return(max(possibilities))
-
-
-with open(sys.argv[1]) as f:
-    lines = f.readlines()
-    for line in lines:
-        elem = line.split(" ")
-        hand = elem[0]
-        bid = int(elem[1])
-        type = best_score(hand)
-                
-        hands.append((hand, bid, type))
-
-
-hands = sorted(hands, key=lambda word: (word[2], [a[::-1].index(c) for c in word[0]]))
-winnings = 0
-for (index, hand) in enumerate(hands):
-    winnings += hand[1] * (index+1)
-print("Total winnings: ", winnings)
 
